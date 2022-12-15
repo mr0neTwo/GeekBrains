2f8cc44231ad5cc11ef24599cf314ff95e1afa04
Stanislav_Kaygorodov <stasmen@mail.ru>
2022-12-15 11:37:32 +0300

    some changes

---
+++ Example002/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите число");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+
+Console.WriteLine("Квадрат числа " + number + " будет " + result);
