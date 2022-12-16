8afb6e43486c04505f8c64df4384e952e64a3c20
Stanislav_Kaygorodov <stasmen@mail.ru>
2022-12-16 12:13:38 +0300

    Added C# HomeWork001 Task08

--- Home_works/C#/HomeWork001/Task08/Program.cs
+++ Home_works/C#/HomeWork001/Task08/Program.cs
@@ -4,3 +4,28 @@
 5 -> 2, 4
 8 -> 2, 4, 6, 8
 */
+
+static int GetNumberFromConsole(string massage)
+{
+    int number;
+    while (true)
+    {
+        Console.Write(massage);
+    
+        string input = Console.ReadLine();
+        if (int.TryParse(input, out number)) break;
+    
+        Console.WriteLine("Число должно быть целым!");
+    }
+
+    return number;
+}
+
+int number = GetNumberFromConsole("Введите целое число: ");
+
+Console.Write("{");
+for (int i = 2; i <= number; i += 2)
+{
+    Console.Write(i + (i == number | i == number - 1 ? "" : ", " ));
+}
+Console.WriteLine("}");
