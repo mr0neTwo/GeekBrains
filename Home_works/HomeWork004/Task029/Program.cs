7e1fb092d114ba3cc31c1c41ed76a919da6d5e78
Stanislav_Kaygorodov <stasmen@mail.ru>
2023-01-13 12:33:30 +0300

    HomeWork4

--- Home_works/HomeWork004/Task029/Program.cs
+++ Home_works/HomeWork004/Task029/Program.cs
@@ -1,3 +1,47 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
+
+// Так как в условии не понятно, откуда именно программа берет элементы массива, а уточнять долго, два варианта:
+
+static int GetIntFromConsole(string massage)
+{
+    int number = 0;
+    bool check = true;
+    while (check)
+    {
+        Console.Write(massage);
+
+        string input = Console.ReadLine()!;
+        if (int.TryParse(input, out number))
+        {
+            check = false;
+        }
+        else
+        {
+            Console.WriteLine("Число должно быть целым!");
+        }
+    }
+
+    return number;
+}
+
+static void MakeAndPrintArray(bool isRandom = false)
+{
+    int  arrayLength = 8;
+    int[] array = new int[arrayLength];
+    var random = new Random();
+
+    for (int i = 0; i < arrayLength; i++)
+    {
+        array[i] = isRandom ? random.Next(0, 100) : array[i] = GetIntFromConsole($"Введите число {i + 1}: ");
+    }
+
+    Console.WriteLine($" [ {String.Join(", ", array)} ] ");
+}
+
+// Вариант 1. Случайные элементы.
+MakeAndPrintArray(isRandom: true);
+
+// Вариант 2. Элементы вводятся с консоли.
+MakeAndPrintArray();
